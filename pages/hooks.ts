1b53ee6a0b8adaac1254510293c495158cdeb71e
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-08-09 15:00:22 +0800

    add tailwind and `useFundWallet` (doesnt work lmao)

--- pages/hooks.ts
+++ pages/hooks.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContext } from './components/Provider';
 
 export const useBalance = () => {
@@ -10,4 +10,36 @@ export const useBalance = () => {
   };
 
   return { balance, fetchBalance };
+};
+
+export const useBundlrInstance = () => {
+  const { bundlr } = useContext(AppContext);
+
+  return { bundlr };
+};
+
+export const useFundWallet = () => {
+  const { bundlr } = useContext(AppContext);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fundWallet = async (amount: string) => {
+    try {
+      setLoading(true);
+      if (!bundlr) {
+        alert('bundlr 404');
+      }
+      await bundlr?.fund(amount);
+    } catch (error) {
+      setError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    fundWallet,
+    loading,
+    error,
+  };
 };
